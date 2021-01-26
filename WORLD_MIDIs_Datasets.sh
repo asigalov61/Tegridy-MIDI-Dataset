c34936213196338ba962d73ac4a05000ce561fde
Alex <asigalov61@hotmail.com>
2021-01-26 14:44:16 -0800

    Update WORLD_MIDIs_Datasets.sh

--- WORLD_MIDIs_Datasets.sh
+++ WORLD_MIDIs_Datasets.sh
@@ -7,3 +7,13 @@ wget --no-check-certificate -O Melody-ABC-Tunes-Dataset-CC-BY-NC-SA.zip "https:/
 
 # WORLD_MIDIs_Dataset-CC-BY-NC-SA
 wget --no-check-certificate -O WORLD_MIDIs_Dataset-CC-BY-NC-SA.zip "https://onedrive.live.com/download?cid=8A0D502FC99C608F&resid=8A0D502FC99C608F%2118452&authkey=AHzh7Kf2e_58Az8"
+
+# Processed MIDI datasets for World MIDIs explorer
+# https://github.com/asigalov61/tegridy-tools/blob/main/tegridy-tools/notebooks/WORLD_MIDIs_Explorer.ipynb
+
+# World-Melody-ABC-Tunes-Dataset
+wget --no-check-certificate -O World-Melody-ABC-Tunes-Dataset.zip "https://onedrive.live.com/download?cid=8A0D502FC99C608F&resid=8A0D502FC99C608F%2118455&authkey=AFoi-2OWbh3vqms"
+
+# WORLD_MIDIs_Dataset
+wget --no-check-certificate -O WORLD_MIDIs_Dataset.zip "https://onedrive.live.com/download?cid=8A0D502FC99C608F&resid=8A0D502FC99C608F%2118459&authkey=AAJauCtRPptxS_Y"
+
