86bdd50cb8f475fc54b3addfbb50b23a094d419c
Alex <asigalov61@hotmail.com>
2021-01-26 14:33:10 -0800

    WORLD MIDIs Datasets Download Bash Script

---
+++ WORLD_MIDIs_Datasets.sh
@@ -0,0 +1,9 @@
+# WORLD MIDIs Datasets
+
+# Original MIDIs
+
+# Melody-ABC-Tunes-Dataset-CC-BY-NC-SA
+wget --no-check-certificate -O Melody-ABC-Tunes-Dataset-CC-BY-NC-SA.zip "https://onedrive.live.com/download?cid=8A0D502FC99C608F&resid=8A0D502FC99C608F%2118458&authkey=AOXe4AaDtageB-k"
+
+# WORLD_MIDIs_Dataset-CC-BY-NC-SA
+wget --no-check-certificate -O WORLD_MIDIs_Dataset-CC-BY-NC-SA.zip "https://onedrive.live.com/download?cid=8A0D502FC99C608F&resid=8A0D502FC99C608F%2118452&authkey=AHzh7Kf2e_58Az8"
